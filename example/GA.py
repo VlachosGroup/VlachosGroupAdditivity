b7251f535c76c9e2c83353385afb08296ddcc797
Gerhard R Wittreich, P.E <wittregr@udel.edu>
2019-08-14 12:33:49 -0400

    init docs

--- example/GA.py
+++
@@ -1,8 +0,0 @@
-from pgradd.GroupAdd.Library import GroupLibrary
-import pgradd.ThermoChem
-lib = GroupLibrary.Load('GuSolventGA2017Aq')
-descriptors = lib.GetDescriptors('C(=O)([Pt])O')
-#descriptors = lib.GetDescriptors('C=C')
-print(descriptors)
-thermochem = lib.Estimate(descriptors,'thermochem')
-print((thermochem.get_HoRT(500)))
